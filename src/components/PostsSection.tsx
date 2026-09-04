4adcea8998b4e80245cd13e320c124f749a56c8c
edfherae <163173239+edfherae@users.noreply.github.com>
2026-09-04 17:01:24 +0300

    added comment fetching on post click

--- src/components/PostsSection.tsx
+++ src/components/PostsSection.tsx
@@ -1,5 +1,6 @@
+import { useEffect, useRef, useState } from "react";
 import useFetch from "../hooks/useFetch";
-import type { Post } from "../models/models";
+import type { Post, Comment } from "../models/models";
 
 export default function PostsSection({
   tab,
@@ -11,6 +12,11 @@ export default function PostsSection({
     [tab],
   );
 
+  const [selectedPostId, setSelectedPostId] = useState<number | null>(null);
+  const fetchComment = useFetch<Comment[]>(
+    `https://jsonplaceholder.typicode.com/comments?postId=${selectedPostId}`,
+  );
+
   return (
     <>
       {isLoading && <p>Loading...</p>}
@@ -18,9 +24,28 @@ export default function PostsSection({
       {!isLoading && data && (
         <div>
           {data.map((post) => (
-            <div className="card" key={post.id}>
+            <div
+              className="card"
+              key={post.id}
+              onClick={() => setSelectedPostId(post.id)}
+            >
               <h3>{post.title}</h3>
               <p>{post.body}</p>
+
+              {selectedPostId === post.id && <h3>Comments:</h3>}
+              {selectedPostId === post.id && fetchComment.isLoading && (
+                <p>Loading...</p>
+              )}
+              {selectedPostId === post.id &&
+                !fetchComment.isLoading &&
+                fetchComment.data &&
+                fetchComment.data.map((comment) => (
+                  <div className="card" key={comment.id}>
+                    <p>User: {comment.email}</p>
+                    <h3>{comment.name}</h3>
+                    <p>{comment.body}</p>
+                  </div>
+                ))}
             </div>
           ))}
         </div>
