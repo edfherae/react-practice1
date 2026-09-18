fb74fb87647739a61894c1223f3548e21cdb175e
edfherae <163173239+edfherae@users.noreply.github.com>
2026-09-18 12:25:10 +0300

    feat: add redirect from content/ to content/users

--- src/pages/ContentPage.tsx
+++ src/pages/ContentPage.tsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import UsersSection from "../components/UsersSection";
 import PostsSection from "../components/PostsSection";
 import CommentsSection from "../components/CommentsSection";
 import Tab from "../components/Tab";
 import type { TabName } from "../types";
+import { useEffect } from "react";
 
 export default function ContentPage() {
   const { tab } = useParams<{ tab: TabName }>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!tab) navigate("users", { replace: true });
+  }, [tab]);
 
   return (
     <>
